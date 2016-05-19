19935aeacff510f7d13396565354be79099f9076
Levvy <mizyn24@gmail.com>
2016-05-19 13:45:12 +0200

    Gems updates, fixed restart_dev script

--- restartdb_dev.sh
+++ restartdb_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rake db:drop RAILS_ENV="production" 
-rake db:create RAILS_ENV="production" 
+rake db:drop RAILS_ENV="development" 
+rake db:create RAILS_ENV="development" 
 rake db:migrate RAILS_ENV="development" 
-rake db:seed RAILS_ENV="production" 
+rake db:seed RAILS_ENV="development" 
 
